1df8eec6e1e3c2034e73068d2f60c192efa09e86
hyeonDD <selwha1@naver.com>
2024-10-04 15:30:24 +0900

    fix rename

--- test_script/basic_graph/walk.sh
+++ test_script/basic_graph/walk.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-LOG_DIR="/home/aiffelzero/zero/humanoid/humanoid-bench/tdmpc2/logs"
-
 # hurdle
-nohup python -m tdmpc2.train exp_name=tdmpc task=humanoid_h1-walk_with_hurdle-v0 seed=0 disable_wandb=False wandb_entity=zerobeak wandb_project=default_walk_with_hurdle > $LOG_DIR/nohup_walk.out &
+nohup python -m tdmpc2.train exp_name=tdmpc task=humanoid_h1-walk_with_hurdle-v0 seed=0 disable_wandb=False wandb_entity=zerobeak wandb_project=default_walk_with_hurdle > logs/nohup_walk_with_hurdle.out &
 # pole
-nohup python -m tdmpc2.train exp_name=tdmpc task=humanoid_h1-walk_with_pole-v0 seed=0 disable_wandb=False wandb_entity=zerobeak wandb_project=default_walk_with_pole > $LOG_DIR/nohup_walk.out &
+nohup python -m tdmpc2.train exp_name=tdmpc task=humanoid_h1-walk_with_pole-v0 seed=0 disable_wandb=False wandb_entity=zerobeak wandb_project=default_walk_with_pole > logs/nohup_walk_with_pole.out &
 # stair
-nohup python -m tdmpc2.train exp_name=tdmpc task=humanoid_h1-walk_with_stair-v0 seed=0 disable_wandb=False wandb_entity=zerobeak wandb_project=default_walk_with_stair > $LOG_DIR/nohup_walk.out &
+nohup python -m tdmpc2.train exp_name=tdmpc task=humanoid_h1-walk_with_stair-v0 seed=0 disable_wandb=False wandb_entity=zerobeak wandb_project=default_walk_with_stair > logs/nohup_walk_with_stair.out &
