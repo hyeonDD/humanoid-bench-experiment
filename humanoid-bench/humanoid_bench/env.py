4f23303de7ac61777382c27f2a337f0be756a0b2
JooHyun <silpiria98@outlook.com>
2024-10-10 12:05:23 +0900

    debuging random start

--- humanoid-bench/humanoid_bench/env.py
+++ humanoid-bench/humanoid_bench/env.py
@@ -293,7 +293,8 @@ class HumanoidEnv(MujocoEnv, gym.utils.EzPickle):
         # x 값을 0(pole), 19(stair), 27(hurdle) 중 하나로 랜덤하게 선택
         x_position = rng.choice([0, 19, 27])
         self.data.qpos[0] = x_position
-
+        print(f"x_position : {x_position}")
+        print(f"qpos : {self.data.qpos}")
         # 시뮬레이터 업데이트
         mujoco.mj_forward(self.model, self.data)
 
