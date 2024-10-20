1cead10aa358969f6587d4e6e06fd52fde336c90
soysoj <soysoj1810@gmail.com>
2024-10-21 03:42:39 +0900

    Created reward functions with visual image

--- humanoid-bench/humanoid_bench/env.py
+++ humanoid-bench/humanoid_bench/env.py
@@ -56,7 +56,7 @@ from .envs.insert import Insert
 from .envs.custom_task.baseline.custom_task_combine import CustomTaskCombine
 from .envs.custom_task.baseline.base_v2 import Base
 from .envs.custom_task.base_v2_exp_early_terminate import BaseEarlyTerminate
-
+from .envs.custom_task.reward_with_visual import BaseWithTask
 
 DEFAULT_CAMERA_CONFIG = {
     "trackbodyid": 1,
@@ -112,6 +112,7 @@ TASKS = {
     "proto": Base,
     "proto_slide": Base,
     "proto_slide_early_terminate": BaseEarlyTerminate,
+    "proto_with_visual": BaseWithTask,
     #     "walk_with_hurdle": Walk, # basic graph
     #     "walk_with_stair": Walk, # basic graph
     #     "walk_with_pole": Walk, # basic graph
